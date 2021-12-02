355b344e2afdc48410c876f926d926fc9f13ac10
Bartłomiej Stępień <bartek.bartlomiej.96@gmail.com>
2021-12-02 14:56:56 +0100

    [2020/02] Add solution

---
+++ 2020/02.lua
@@ -0,0 +1,66 @@
+local Utils = require("utils")
+
+
+getmetatable("").__index = function (str, key) return string.sub(str, key, key) end
+
+
+function count_occurences(str, character)
+    local _, count = string.gsub(str, character, character)
+    return count
+end
+
+
+function count_valid(input, is_valid)
+    local valid_amount = 0
+
+    local pattern = "(%d+)-(%d+) (%a): (%S+)"
+    for first, second, character, password in string.gmatch(input, pattern) do
+        if is_valid(password, character, tonumber(first), tonumber(second)) then
+            valid_amount = valid_amount + 1
+        end
+    end
+
+    return valid_amount
+end
+
+
+function part_one(input)
+
+    function is_valid(password, character, low, high)
+        local count = count_occurences(password, character)
+        return low <= count and count <= high 
+    end
+
+    return count_valid(input, is_valid)
+end
+
+
+function part_two(input)
+    
+    function is_valid(password, character, first_position, second_position)
+        local first_character = password[first_position]
+        local second_character = password[second_position]
+    
+        if first_character == character then
+            return second_character ~= character
+        elseif second_character == character then
+            return first_character ~= character
+        end
+    
+        return false
+    end
+
+    return count_valid(input, is_valid)
+end
+
+
+local PUZZLE = {
+    year = 2020,
+    day = 2
+}
+
+Utils.check(PUZZLE, part_one, 2, Utils.read_raw)
+Utils.run(PUZZLE, part_one, Utils.read_raw)
+
+Utils.check(PUZZLE, part_two, 1, Utils.read_raw)
+Utils.run(PUZZLE, part_two, Utils.read_raw)

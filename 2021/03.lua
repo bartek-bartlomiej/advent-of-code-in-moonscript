67518ca922579fde1a38d950dc5b7fba59ded3ca
Bartłomiej Stępień <bartek.bartlomiej.96@gmail.com>
2021-12-03 21:36:28 +0100

    [2021/03] Add solution to second part

--- 2021/03.lua
+++ 2021/03.lua
@@ -2,20 +2,21 @@ local Utils = require("utils")
 
 
 local ONE = "1"
+local ZERO = "0"
 
 
 local function part_one(input)
     local length = #(input[1])
 
-    local ones_amount = {}
+    local set_bits_amount = {}
     for i = 1, length do
-        table.insert(ones_amount, 0)
+        table.insert(set_bits_amount, 0)
     end
 
     for _, line in ipairs(input) do
         for i = 1, length do
             if line[i] == ONE then
-                ones_amount[i] = ones_amount[i] + 1
+                set_bits_amount[i] = set_bits_amount[i] + 1
             end
         end
     end
@@ -24,7 +25,7 @@ local function part_one(input)
 
     local rates = {0, 0}
     for i = 1, length do
-        local x = ones_amount[i] > half and 0 or 1
+        local x = set_bits_amount[i] > half and 0 or 1
         local affected, unaffected = 1 + x, 2 - x
 
         rates[affected] = (rates[affected] << 1) + 1
@@ -36,7 +37,53 @@ end
 
 
 local function part_two(input)
-    return 230
+
+    local function count(t, condition)
+        local result = 0
+        for _, value in ipairs(t) do
+            if condition(value) then
+                result = result + 1
+            end
+        end
+
+        return result
+    end
+
+    local function filter(t, condition)
+        local result = {}
+        for _, value in ipairs(t) do
+            if condition(value) then
+                table.insert(result, value)
+            end
+        end
+
+        return result
+    end
+
+    local function get_rating(numbers, most_common)
+        local function get(t, i, x, y)
+            if #t == 1 then
+                return t[1]
+            end
+
+            if #t == 0 or i > #(t[1]) then
+                error("Rating not found")
+            end
+
+            local set_bits_amount = count(t, function (v) return v[i] == ONE end)
+            local filter_value = set_bits_amount >= #t / 2 and x or y
+            local filtered_numbers = filter(t, function (v) return v[i] == filter_value end)
+            return get(filtered_numbers, i + 1, x, y)
+        end
+
+        local values = most_common and { ZERO, ONE } or { ONE, ZERO }
+        return tonumber(get(numbers, 1, table.unpack(values)), 2)
+    end
+
+    local oxygen_generator_rating = get_rating(input, true)
+    local CO2_scrubber_rating = get_rating(input, false)
+
+    return oxygen_generator_rating * CO2_scrubber_rating
 end
 
 
